295272a9e0a37b5749d22650651bb24bfb2bc1f8
Palmer, WR <56736859+palmerwjr93@users.noreply.github.com>
2019-11-01 15:28:30 -0400

    Update AnalyzeHct-Mch.R

--- scripts/AnalyzeHct-Mch.R
+++ scripts/AnalyzeHct-Mch.R
@@ -1,115 +1,39 @@
+##BTEC330 Warren Project2
+
 ## Install necessary packages
 
 install.packages("ggplot2")
 library(ggplot2)
 
-install.packages("scatterplot3d")
-library(scatterplot3d)
 
 ## Read data
 IBS1 <- read.csv("data/RobinsonEtAl_Sup1.csv", header = TRUE)
 head(IBS1)
 write.csv(IBS1, "data_output/output.csv")
 
-##  Single Regressions for BMI vs. each blood data variable
+##  Single Regressions for BMI vs. Hct/Mch
 ##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
 ##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
 ##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
 ##  http://r-statistics.co/Linear-Regression.html
 
-
-single.regression <- lm(BMI ~ SerumCortisol, data=IBS1)
+##Single Regression Test
+single.regression <- lm(BMI ~ HCT, data=IBS1)
 summary(single.regression)
 
-single.regression <- lm(BMI ~ CRP, data=IBS1)
+single.regression <- lm(BMI ~ MCH, data=IBS1)
 summary(single.regression)
 
-single.regression <- lm(BMI ~ ESR, data=IBS1)
-summary(single.regression)
-
-single.regression <- lm(BMI ~ PlateletCount, data=IBS1)
-summary(single.regression)
-
-single.regression <- lm(BMI ~ IgA, data=IBS1)
-summary(single.regression)
-
-single.regression <- lm(BMI ~ Lymphocytes, data=IBS1)
-summary(single.regression)
-
-## Multiple Regressions
-
-multiple.regression <- lm(BMI ~ SerumCortisol + CRP, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + ESR, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + ESR, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + PlateletCount, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + ACTH, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Lymphocytes, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgA, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Monocytes_PCT, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Monocytes, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Lymphocytes_PCT, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgG, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgE, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgM, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Neutrophils, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Neutrophil_PCT, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IL10, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + LBP, data=IBS1)
-summary(multiple.regression)
-
-##Select the best 3-variable regression model based on highest scores from previous tests
-
-fit1 <- lm(BMI ~ SerumCortisol + CRP + ESR + PlateletCount + Lymphocytes , data=IBS1)
-summary(fit1)
-
-fit1 <- lm(BMI ~ SerumCortisol + CRP + ESR, data=IBS1)
-summary(fit1)
-
-fit1 <- lm(BMI ~ SerumCortisol + CRP, data=IBS1)
-summary(fit1)
-print(fit1)
 
 ## Scatterplots
 ## https://www.statmethods.net/graphs/scatterplot.html
 
 
-ggplot(IBS1, aes(x=BMI, y=SerumCortisol)) +
+ggplot(IBS1, aes(x=BMI, y=HCT)) +
   geom_point() +    
   geom_smooth(method=lm) 
 
-ggplot(IBS1, aes(x=BMI, y=CRP)) +
+ggplot(IBS1, aes(x=BMI, y=MCH)) +
   geom_point() +    
   geom_smooth(method=lm)  
 
